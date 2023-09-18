c74389bcc6c4e84341cc136d8b503348636b5e50
Carmelo Fiorello <carmelofiorello@gmail.com>
2023-09-19 00:08:43 +0200

    Add alarms and temperature

--- esp32m/include/esp32m/dev/rtc.hpp
+++ esp32m/include/esp32m/dev/rtc.hpp
@@ -3,34 +3,37 @@
 #include "esp32m/bus/i2c.hpp"
 #include "esp32m/device.hpp"
 
-namespace esp32m
-{
+namespace esp32m {
   class Rtc;
 
-  namespace rtc
-  {
+  namespace rtc {
 
     const uint8_t DefaultAddress = 0x68;
 
-    const int days_per_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    const int days_per_month_leap_year[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    const int days_per_month[] = {31, 28, 31, 30, 31, 30,
+                                  31, 31, 30, 31, 30, 31};
+    const int days_per_month_leap_year[] = {31, 29, 31, 30, 31, 30,
+                                            31, 31, 30, 31, 30, 31};
 
     /**
      * Alarms
      */
-    enum Alarm
-    {
-      DS3231_ALARM_NONE = 0, //!< No alarms
-      DS3231_ALARM_1,        //!< First alarm
-      DS3231_ALARM_2,        //!< Second alarm
-      DS3231_ALARM_BOTH      //!< Both alarms
+    enum AlarmFlag { DS3231_SET = 0, DS3231_CLEAR, DS3231_REPLACE };
+
+    /**
+     * Alarms
+     */
+    enum Alarm {
+      DS3231_ALARM_NONE = 0,  //!< No alarms
+      DS3231_ALARM_1,         //!< First alarm
+      DS3231_ALARM_2,         //!< Second alarm
+      DS3231_ALARM_BOTH       //!< Both alarms
     };
 
     /**
      * First alarm rate
      */
-    enum Alarm1Rate
-    {
+    enum Alarm1Rate {
       DS3231_ALARM1_EVERY_SECOND = 0,
       DS3231_ALARM1_MATCH_SEC,
       DS3231_ALARM1_MATCH_SECMIN,
@@ -42,8 +45,7 @@ namespace esp32m
     /**
      * Second alarm rate
      */
-    enum Alarm2Rate
-    {
+    enum Alarm2Rate {
       DS3231_ALARM2_EVERY_MIN = 0,
       DS3231_ALARM2_MATCH_MIN,
       DS3231_ALARM2_MATCH_MINHOUR,
@@ -54,16 +56,14 @@ namespace esp32m
     /**
      * Squarewave frequency
      */
-    enum SquarewaveFrequency
-    {
+    enum SquarewaveFrequency {
       DS3231_SQWAVE_1HZ = 0x00,
       DS3231_SQWAVE_1024HZ = 0x08,
       DS3231_SQWAVE_4096HZ = 0x10,
       DS3231_SQWAVE_8192HZ = 0x18
     };
 
-    enum Register
-    {
+    enum Register {
       DS3231_STAT_OSCILLATOR = 0x80,
       DS3231_STAT_32KHZ = 0x08,
       DS3231_STAT_ALARM_2 = 0x02,
@@ -88,63 +88,190 @@ namespace esp32m
       DS3231_MONTH_MASK = 0x1f
     };
 
-    struct Settings
-    {
-    public:
+    struct Settings {
+     public:
       uint8_t getInt() const;
       void setInt(uint8_t value);
 
-    private:
+     private:
       uint8_t _int = 0;
       friend class Core;
     };
 
-    class Core : public virtual log::Loggable
-    {
-    public:
+    class Core : public virtual log::Loggable {
+     public:
       Core(I2C *i2c, const char *name);
       Core(const Core &) = delete;
-      const char *name() const override
-      {
+      const char *name() const override {
         return _name;
       }
-      Settings &settings()
-      {
+      Settings &settings() {
         return _settings;
       }
+
+      /**
+       * @brief Set the time on the RTC
+       *
+       * Timezone agnostic, pass whatever you like.
+       * I suggest using GMT and applying timezone and DST when read back.
+       *
+       * @return ESP_OK to indicate success
+       */
       esp_err_t setTime(struct tm *tv);
+
+      /**
+       * @brief Get the time from the RTC, populates a supplied tm struct
+       *
+       * @param dev Device descriptor
+       * @param[out] time RTC time
+       * @return ESP_OK to indicate success
+       */
       esp_err_t getTime(struct tm *tv);
 
-    protected:
-      uint8_t dec2bcd(uint8_t val);
+      /**
+       * @brief Set alarms
+       *
+       * `alarm1` works with seconds, minutes, hours and day of week/month, or
+       * fires every second. `alarm2` works with minutes, hours and day of
+       * week/month, or fires every minute.
+       *
+       * Not all combinations are supported, see `DS3231_ALARM1_*` and
+       * `DS3231_ALARM2_*` defines for valid options you only need to populate
+       * the fields you are using in the `tm` struct, and you can set both
+       * alarms at the same time (pass
+       * `DS3231_ALARM_1`/`DS3231_ALARM_2`/`DS3231_ALARM_BOTH`).
+       *
+       * If only setting one alarm just pass 0 for `tm` struct and `option`
+       * field for the other alarm. If using
+       * ::DS3231_ALARM1_EVERY_SECOND/::DS3231_ALARM2_EVERY_MIN you can pass 0
+       * for `tm` struct.
+       *
+       * If you want to enable interrupts for the alarms you need to do that
+       * separately.
+       *
+       * @return ESP_OK to indicate success
+       */
+      esp_err_t ds3231_set_alarm(Alarm alarms, struct tm *time1,
+                                 Alarm1Rate option1, struct tm *time2,
+                                 Alarm2Rate option2);
+
+      /**
+       * @brief Check which alarm(s) have past
+       *
+       * Sets alarms to
+       * `DS3231_ALARM_NONE`/`DS3231_ALARM_1`/`DS3231_ALARM_2`/`DS3231_ALARM_BOTH`
+       *
+       * @param dev Device descriptor
+       * @param[out] alarms Alarms
+       * @return ESP_OK to indicate success
+       */
+      esp_err_t ds3231_get_alarm_flags(Alarm *alarms);
+
+      /**
+       * @brief Clear alarm past flag(s)
+       *
+       * Pass `DS3231_ALARM_1`/`DS3231_ALARM_2`/`DS3231_ALARM_BOTH`
+       *
+       * @param dev Device descriptor
+       * @param alarms Alarms
+       * @return ESP_OK to indicate success
+       */
+      esp_err_t ds3231_clear_alarm_flags(Alarm alarms);
+
+      /**
+       * @brief enable alarm interrupts (and disables squarewave)
+       *
+       * Pass `DS3231_ALARM_1`/`DS3231_ALARM_2`/`DS3231_ALARM_BOTH`.
+       *
+       * If you set only one alarm the status of the other is not changed
+       * you must also clear any alarm past flag(s) for alarms with
+       * interrupt enabled, else it will trigger immediately.
+       *
+       * @param dev Device descriptor
+       * @param alarms Alarms
+       * @return ESP_OK to indicate success
+       */
+      esp_err_t ds3231_enable_alarm_ints(Alarm alarms);
+
+      /**
+       * @brief Disable alarm interrupts
+       *
+       * Does not (re-)enable squarewave
+       *
+       * @param dev Device descriptor
+       * @param alarms Alarm
+       * @return ESP_OK to indicate success
+       */
+      esp_err_t ds3231_disable_alarm_ints(Alarm alarms);
+
+      /**
+       * @brief Get the raw temperature value
+       *
+       * **Supported only by DS3231**
+       *
+       * @param dev Device descriptor
+       * @param[out] temp Raw temperature value
+       * @return ESP_OK to indicate success
+       */
+      esp_err_t ds3231_get_raw_temp(int16_t *temp);
+
+      /**
+       * @brief Get the temperature as an integer
+       *
+       * **Supported only by DS3231**
+       *
+       * @param dev Device descriptor
+       * @param[out] temp Temperature, degrees Celsius
+       * @return ESP_OK to indicate success
+       */
+      esp_err_t ds3231_get_temp_integer(int8_t *temp);
+
+      /**
+       * @brief Get the temperature as a float
+       *
+       * **Supported only by DS3231**
+       *
+       * @param dev Device descriptor
+       * @param[out] temp Temperature, degrees Celsius
+       * @return ESP_OK to indicate success
+       */
+      esp_err_t ds3231_get_temp_float(float *temp);
+
+     protected:
       std::unique_ptr<I2C> _i2c;
 
-    private:
+     private:
       const char *_name;
       bool _inited = false;
       Settings _settings;
       friend class esp32m::Rtc;
-    };
-  } // namespace rtc
 
-  namespace dev
-  {
-    class Rtc : public virtual Device, public virtual rtc::Core
-    {
-    public:
+      /* Set/clear bits in a byte register, or replace the byte altogether
+       * pass the register address to modify, a byte to replace the existing
+       * value with or containing the bits to set/clear and one of
+       * DS3231_SET/DS3231_CLEAR/DS3231_REPLACE
+       * returns true to indicate success
+       */
+      esp_err_t ds3231_set_flag(Register addr, uint8_t bits, AlarmFlag mode);
+    };
+  }  // namespace rtc
+
+  namespace dev {
+    class Rtc : public virtual Device, public virtual rtc::Core {
+     public:
       Rtc(I2C *i2c, const char *name);
       Rtc(const Rtc &) = delete;
 
-    protected:
+     protected:
       void handleEvent(Event &ev) override;
       DynamicJsonDocument *getState(const JsonVariantConst args) override;
       bool initSensors() override;
       bool pollSensors() override;
 
-    private:
+     private:
       Sensor _currentTime, _alarm1, _alarm2, _temperature;
     };
 
     void useRtc(const char *name = nullptr, uint8_t addr = rtc::DefaultAddress);
-  } // namespace dev
-} // namespace esp32m
+  }  // namespace dev
+}  // namespace esp32m
--- esp32m/src/dev/rtc.cpp
+++ esp32m/src/dev/rtc.cpp
@@ -1,32 +1,28 @@
-#include "esp32m/defs.hpp"
 #include "apps/esp_sntp.h"
+#include "esp32m/defs.hpp"
 
 #include "esp32m/dev/rtc.hpp"
-#include "esp32m/net/sntp.hpp"
 #include "esp32m/ha/ha.hpp"
+#include "esp32m/net/sntp.hpp"
 /**
- * Use this library as template: https://github.com/UncleRus/esp-idf-lib/tree/master/examples/ds3231/default
+ * Use this library as template:
+ * https://github.com/UncleRus/esp-idf-lib/tree/master/examples/ds3231/default
  */
 
-namespace esp32m
-{
+namespace esp32m {
 
-  namespace rtc
-  {
+  namespace rtc {
 
-    uint8_t Settings::getInt() const
-    {
+    uint8_t Settings::getInt() const {
       return _int;
     }
-    void Settings::setInt(uint8_t value)
-    {
+    void Settings::setInt(uint8_t value) {
       if (_int == value)
         return;
       _int = value;
     }
 
-    Core::Core(I2C *i2c, const char *name) : _i2c(i2c)
-    {
+    Core::Core(I2C *i2c, const char *name) : _i2c(i2c) {
       _name = name;
       // _i2c->setEndianness(Endian::Little);
       // _i2c->setErrSnooze(30000);
@@ -48,21 +44,18 @@ namespace esp32m
     }
 
     // TODO: Find a better way to organize this
-    static uint8_t dec2bcd(uint8_t val)
-    {
+    static uint8_t dec2bcd(uint8_t val) {
       return ((val / 10) << 4) + (val % 10);
     }
 
     // TODO: Find a better way to organize this
-    static uint8_t bcd2dec(uint8_t val)
-    {
+    static uint8_t bcd2dec(uint8_t val) {
       return (val >> 4) * 10 + (val & 0x0f);
     }
 
     // TODO: Find a better way to organize this
     // Function to convert year, month, and day to days since January 1st
-    static inline int days_since_january_1st(int year, int month, int day)
-    {
+    static inline int days_since_january_1st(int year, int month, int day) {
       int days = day - 1;
       const int *ptr = days_per_month;
 
@@ -71,16 +64,14 @@ namespace esp32m
         ptr = days_per_month_leap_year;
 
       // Add days from previous months
-      for (int i = 0; i < month; i++)
-      {
+      for (int i = 0; i < month; i++) {
         days += ptr[i];
       }
 
       return days;
     }
 
-    esp_err_t Core::setTime(struct tm *time)
-    {
+    esp_err_t Core::setTime(struct tm *time) {
       // if (!_inited)
       //   return ESP_ERR_INVALID_STATE;
       uint8_t data[7];
@@ -102,8 +93,7 @@ namespace esp32m
       return ESP_OK;
     }
 
-    esp_err_t Core::getTime(struct tm *time)
-    {
+    esp_err_t Core::getTime(struct tm *time) {
       // if (!_inited)
       //   return ESP_ERR_INVALID_STATE;
       uint8_t data[7];
@@ -114,25 +104,24 @@ namespace esp32m
       /* convert to unix time structure */
       time->tm_sec = rtc::bcd2dec(data[0]);
       time->tm_min = rtc::bcd2dec(data[1]);
-      if (data[2] & DS3231_12HOUR_FLAG)
-      {
+      if (data[2] & DS3231_12HOUR_FLAG) {
         /* 12H */
         time->tm_hour = rtc::bcd2dec(data[2] & DS3231_12HOUR_MASK) - 1;
         /* AM/PM? */
         if (data[2] & DS3231_PM_FLAG)
           time->tm_hour += 12;
-      }
-      else
+      } else
         time->tm_hour = rtc::bcd2dec(data[2]); /* 24H */
       time->tm_wday = rtc::bcd2dec(data[3]) - 1;
       time->tm_mday = rtc::bcd2dec(data[4]);
       time->tm_mon = rtc::bcd2dec(data[5] & DS3231_MONTH_MASK) - 1;
       time->tm_year = rtc::bcd2dec(data[6]) + 100;
       time->tm_isdst = 0;
-      time->tm_yday = rtc::days_since_january_1st(time->tm_year, time->tm_mon, time->tm_mday);
+      time->tm_yday = rtc::days_since_january_1st(time->tm_year, time->tm_mon,
+                                                  time->tm_mday);
 
-      // apply a time zone (if you are not using localtime on the rtc or you want to check/apply DST)
-      // applyTZ(time);
+      // apply a time zone (if you are not using localtime on the rtc or you
+      // want to check/apply DST) applyTZ(time);
 
       // Log time
       char buf[32];
@@ -141,18 +130,149 @@ namespace esp32m
       return ESP_OK;
     }
 
-  } // namespace rtc
+    esp_err_t Core::ds3231_set_alarm(Alarm alarms, struct tm *time1,
+                                     Alarm1Rate option1, struct tm *time2,
+                                     Alarm2Rate option2) {
+      int i = 0;
+      uint8_t data[7];
 
-  namespace dev
-  {
+      /* alarm 1 data */
+      if (alarms != DS3231_ALARM_2) {
+        data[i++] =
+            (option1 >= DS3231_ALARM1_MATCH_SEC ? rtc::dec2bcd(time1->tm_sec)
+                                                : DS3231_ALARM_NOTSET);
+        data[i++] =
+            (option1 >= DS3231_ALARM1_MATCH_SECMIN ? rtc::dec2bcd(time1->tm_min)
+                                                   : DS3231_ALARM_NOTSET);
+        data[i++] = (option1 >= DS3231_ALARM1_MATCH_SECMINHOUR
+                         ? rtc::dec2bcd(time1->tm_hour)
+                         : DS3231_ALARM_NOTSET);
+        data[i++] =
+            (option1 == DS3231_ALARM1_MATCH_SECMINHOURDAY
+                 ? (rtc::dec2bcd(time1->tm_wday + 1) & DS3231_ALARM_WDAY)
+                 : (option1 == DS3231_ALARM1_MATCH_SECMINHOURDATE
+                        ? rtc::dec2bcd(time1->tm_mday)
+                        : DS3231_ALARM_NOTSET));
+      }
+
+      /* alarm 2 data */
+      if (alarms != DS3231_ALARM_1) {
+        data[i++] =
+            (option2 >= DS3231_ALARM2_MATCH_MIN ? rtc::dec2bcd(time2->tm_min)
+                                                : DS3231_ALARM_NOTSET);
+        data[i++] = (option2 >= DS3231_ALARM2_MATCH_MINHOUR
+                         ? rtc::dec2bcd(time2->tm_hour)
+                         : DS3231_ALARM_NOTSET);
+        data[i++] =
+            (option2 == DS3231_ALARM2_MATCH_MINHOURDAY
+                 ? (rtc::dec2bcd(time2->tm_wday + 1) & DS3231_ALARM_WDAY)
+                 : (option2 == DS3231_ALARM2_MATCH_MINHOURDATE
+                        ? rtc::dec2bcd(time2->tm_mday)
+                        : DS3231_ALARM_NOTSET));
+      }
+
+      ESP_CHECK_LOGW_RETURN(_i2c->write((alarms == Alarm::DS3231_ALARM_2
+                                             ? Register::DS3231_ADDR_ALARM2
+                                             : Register::DS3231_ADDR_ALARM1),
+                                        data, i),
+                            "failed to set time");
+
+      return ESP_OK;
+    }
+
+    esp_err_t Core::ds3231_get_alarm_flags(Alarm *alarms) {
+      uint8_t data;
+
+      /* get register */
+      /* get status register */
+      ESP_CHECK_LOGW_RETURN(_i2c->read(DS3231_ADDR_STATUS, &data, 1),
+                            "failed to get alarm flags");
+
+      /* return only requested flag */
+      *alarms = (Alarm)(data & DS3231_ALARM_BOTH);
+      return ESP_OK;
+    }
+
+    esp_err_t Core::ds3231_clear_alarm_flags(Alarm alarms) {
+      ds3231_set_flag(DS3231_ADDR_STATUS, alarms, DS3231_CLEAR);
+
+      return ESP_OK;
+    }
+
+    esp_err_t Core::ds3231_enable_alarm_ints(Alarm alarms) {
+      ds3231_set_flag(DS3231_ADDR_CONTROL, DS3231_CTRL_ALARM_INTS | alarms,
+                      DS3231_SET);
+
+      return ESP_OK;
+    }
+
+    esp_err_t Core::ds3231_disable_alarm_ints(Alarm alarms) {
+      ds3231_set_flag(DS3231_ADDR_CONTROL, alarms, DS3231_CLEAR);
+
+      return ESP_OK;
+    }
+
+    esp_err_t Core::ds3231_get_raw_temp(int16_t *temp) {
+      uint8_t data[2];
+
+      ESP_CHECK_LOGW_RETURN(_i2c->read(DS3231_ADDR_TEMP, &data, sizeof(data)),
+                            "failed to get raw temp");
+
+      *temp = (int16_t)(int8_t)data[0] << 2 | data[1] >> 6;
+
+      return ESP_OK;
+    }
+
+    esp_err_t Core::ds3231_get_temp_integer(int8_t *temp) {
+      int16_t t_int;
+
+      esp_err_t res = ds3231_get_raw_temp(&t_int);
+      if (res == ESP_OK)
+        *temp = t_int >> 2;
+
+      return res;
+    }
+
+    esp_err_t Core::ds3231_get_temp_float(float *temp) {
+      int16_t t_int;
+
+      esp_err_t res = ds3231_get_raw_temp(&t_int);
+      if (res == ESP_OK)
+        *temp = t_int * 0.25;
+
+      return res;
+    }
+
+    esp_err_t Core::ds3231_set_flag(Register addr, uint8_t bits,
+                                    AlarmFlag mode) {
+      uint8_t data;
+
+      /* get status register */
+      ESP_CHECK_LOGW_RETURN(_i2c->read(addr, &data, 1), "failed to set flag");
+
+      /* clear the flag */
+      if (mode == DS3231_REPLACE)
+        data = bits;
+      else if (mode == DS3231_SET)
+        data |= bits;
+      else
+        data &= ~bits;
+
+      ESP_CHECK_LOGW_RETURN(_i2c->write(addr, &data, 1), "failed to set flag");
+
+      return ESP_OK;
+    }
+
+  }  // namespace rtc
+
+  namespace dev {
 
     Rtc::Rtc(I2C *i2c, const char *name)
         : rtc::Core(i2c, name),
           _currentTime(this, "currentTime"),
           _alarm1(this, "alarm1"),
           _alarm2(this, "alarm2"),
-          _temperature(this, "temperature")
-    {
+          _temperature(this, "temperature") {
       auto group = sensor::nextGroup();
       _currentTime.group = group;
       _currentTime.precision = 2;
@@ -166,17 +286,13 @@ namespace esp32m
       Device::init(Flags::HasSensors);
     }
 
-    bool Rtc::initSensors()
-    {
-      return true; // sync(true) == ESP_OK;
+    bool Rtc::initSensors() {
+      return true;  // sync(true) == ESP_OK;
     }
 
-    void Rtc::handleEvent(Event &ev)
-    {
-      if (EventStateChanged::is(ev, &net::Sntp::instance()))
-      {
-        if (sntp_get_sync_status() == SNTP_SYNC_STATUS_COMPLETED)
-        {
+    void Rtc::handleEvent(Event &ev) {
+      if (EventStateChanged::is(ev, &net::Sntp::instance())) {
+        if (sntp_get_sync_status() == SNTP_SYNC_STATUS_COMPLETED) {
           struct tm timeinfo;
           time_t now;
           time(&now);
@@ -186,8 +302,7 @@ namespace esp32m
       }
     }
 
-    bool Rtc::pollSensors()
-    {
+    bool Rtc::pollSensors() {
       float t = 0, p = 0, h = 0, l = 0;
       // ESP_CHECK_RETURN_BOOL(read(&t, &p, &h));
       sensor("temperature", t);
@@ -207,15 +322,16 @@ namespace esp32m
       return true;
     }
 
-    DynamicJsonDocument *Rtc::getState(const JsonVariantConst args)
-    {
+    DynamicJsonDocument *Rtc::getState(const JsonVariantConst args) {
       float t = 0, p = 0, h = 0, l = 0;
       // if (read(&t, &p, &h) != ESP_OK)
       // return nullptr;
       DynamicJsonDocument *doc = new DynamicJsonDocument(JSON_OBJECT_SIZE(7));
       JsonObject root = doc->to<JsonObject>();
       root["addr"] = _i2c->addr();
-      root["temperature"] = t;
+      int8_t temp;
+      ds3231_get_temp_integer(&temp);
+      root["temperature"] = temp;
       root["alarm1"] = p;
       // if (chipId() == bme280::ChipId::Bme280)
       root["alarm2"] = h;
@@ -229,11 +345,10 @@ namespace esp32m
       return doc;
     }
 
-    void useRtc(const char *name, uint8_t addr)
-    {
+    void useRtc(const char *name, uint8_t addr) {
       new Rtc(new I2C(addr), name);
     }
 
-  } // namespace dev
+  }  // namespace dev
 
-} // namespace esp32m
+}  // namespace esp32m
